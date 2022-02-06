b32fda33c5d99313f330d1d4fbe635ab9dac6753
minericefield <minericefield@gmail.com>
2022-02-07 00:05:11 +0900

    Add: task repository interface

---
+++ src/domain/i-repository/i-task.repository.ts
@@ -0,0 +1,11 @@
+import { Task } from '../domain-object/entity/task';
+import { TaskName } from '../domain-object/value-object/task-name';
+
+export abstract class ITaskRepository {
+  abstract getAll(): Promise<Task[]>;
+  abstract save(task: Task): Promise<void>;
+  abstract updateOne(task: Task): Promise<void>;
+  abstract deleteOne(id: number): Promise<void>;
+  abstract findOneByName(taskName: TaskName): Promise<Task | null>;
+  abstract getNextId(): Promise<number>; // TODO: Possibly not suitable for a repository's responsibility.
+}
